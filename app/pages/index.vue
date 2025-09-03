93e63c7f16adab04a932889742f4d5bef9403bc3
Varanos1 <hnondowou@gmail.com>
2025-09-03 11:33:30 +0200

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-2xl font-thin text-amber-500">Portfolio</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/styles/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+})
